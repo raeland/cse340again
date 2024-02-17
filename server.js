aa989771b45d6a7928cedd93c2b5243248376779
raeland <rsvarady@gmail.com>
2024-02-17 14:03:08 -0500

    error message

--- server.js
+++ server.js
@@ -32,8 +32,11 @@ app.get("/", baseController.buildHome)
 app.use("/inv", inventoryRoute)
 // error route
 app.use("/errors", utilities.error(errorRoute))
-
-
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'SORRY FOR BEING BROKEN'})
+})
+//<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHBqZGVoMTkyMmtrYjhoaHR3Yml6c3d4OTl1a3QzOHE5em1nM25oNCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/h58E0JsuK3h3d8B1do/giphy.gif"
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
