7ba4398e95bebb35ad71b4d744ba5f2227b8247e
raeland <rsvarady@gmail.com>
2024-02-17 10:29:12 -0500

    update inventory folder

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -251,3 +251,9 @@ SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles/'),
     '/thumbnails/',
     '/thumbnails/vehicles/'
   );
+
+UPDATE 
+  public.inventory
+SET 
+  inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
